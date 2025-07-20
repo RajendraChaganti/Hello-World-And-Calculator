3e971709c24fc8366283d20ea9ea9afdf4219380
RajendraChaganti <rajendra2006ch@gmail.com>
2025-07-20 11:10:44 +0530

    Create Experiment-No.1-Hello-world-and-calculator.cpp

---
+++ Experiment-No.1-Hello-world-and-calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // used for input and output
+using namespace std; // so we don't have to write std:: before cin and cout
+
+int main() {
+    cout << "Hello World\n"; // prints Hello World
+
+    float a, b; // declaring two float variables
+
+    cout << "Enter two numbers: "; // asking user for input
+    cin >> a >> b; // taking input for both numbers
+
+    // NOTE: See arrows used in output (cout <<) and input (cin >>) carefully.
+
+    cout << "Addition: " << a + b << endl; // prints sum
+    cout << "Subtraction: " << a - b << endl; // prints difference
+    cout << "Multiplication: " << a * b << endl; // prints product
+    cout << "Division: " << a / b << endl; // prints division 
+
+    return 0; // for sending back the value
+}
